cc69ee6af18d7e50fd45d37eb5c57177ddb752e9
bigdog13 <you@example.com>
2016-07-27 19:32:47 -0400

    add files

---
+++ onparproblem2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace onparproblem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //ask user for 2 number
+            
+         
+        {
+            Console.WriteLine("Enter a number :");
+            int num1 = int.Parse(Console.ReadLine());
+             
+
+            //enter another number
+        Console.WriteLine("Enter a number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            //ask user calculation to be performed
+            Console.WriteLine("What calculation do you want 1 addition 2 subtraction 3 multply");
+
+            
+            {
+                case 1:
+                answer = num1 + num2;
+                Console.WriteLine()
+                break;
+
+                case 2:
+                answer = num1 - num2;
+                break;
+
+                case 3;
+                answer = num1 * num2;
+
+
+                default:
+                break;
+             
+                
+            
+
+            
+            
+
+            
+
+           
+            
+
+            
+
+            
+                
+
+        }
+    }
+}
